9e19c4e4c8353d34c8408fc3d566e33a1cfe0ba9
Weili <weili@grandstream.cn>
2019-11-22 17:36:44 +0800

    Refactor by Spring boot 1. Add the dependencies in pom.xml and remove the not used ones. 2. Add stub docker file

---
+++ astergazer/src/main/java/ua/dborisenko/astergazer/AstergazerApplication.java
@@ -0,0 +1,12 @@
+package ua.dborisenko.astergazer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstergazerApplication.java {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstergazerApplication.java.class, args);
+	}
+}
