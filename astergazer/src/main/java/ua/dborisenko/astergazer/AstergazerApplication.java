0b8b76776f1f019ff4586a6550dccd0c6dfd6c6f
Weili <weili@grandstream.cn>
2019-11-22 18:20:37 +0800

    fix build issue - remove .java from class name.

--- astergazer/src/main/java/ua/dborisenko/astergazer/AstergazerApplication.java
+++ astergazer/src/main/java/ua/dborisenko/astergazer/AstergazerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AstergazerApplication.java {
+public class AstergazerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AstergazerApplication.java.class, args);
